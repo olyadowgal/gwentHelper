2e8dc9e9b06ec5dbc4ecc02347d6b5b3f33b601b
DowgalOlya <dowgalolya@gmail.com>
2019-11-03 17:55:31 +0200

    Add WeatherView

---
+++ app/src/main/java/com/dowgalolya/gwenthelper/widgets/WeatherView.kt
@@ -0,0 +1,20 @@
+package com.dowgalolya.gwenthelper.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import com.dowgalolya.gwenthelper.R
+
+class WeatherView  @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    init {
+        View.inflate(context, R.layout.view_weather, this)
+    }
+
+
+}
