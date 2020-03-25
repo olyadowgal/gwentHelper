782c2c4d803faac05823cf64d7a561efe2b26f83
olyadowgal <dowgalolya@gmail.com>
2020-03-25 12:45:39 +0200

    Two users photo done

--- app/src/main/java/com/dowgalolya/gwenthelper/fragments/MainFragment.kt
+++ app/src/main/java/com/dowgalolya/gwenthelper/fragments/MainFragment.kt
@@ -1,19 +1,28 @@
 package com.dowgalolya.gwenthelper.fragments
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.lifecycle.ViewModelProvider
 import com.dowgalolya.gwenthelper.R
 import com.dowgalolya.gwenthelper.viewmodels.MainViewModel
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.main_fragment.*
 
-class MainFragment : BaseFragment(), View.OnClickListener {
+
+class MainFragment() : BaseFragment(), View.OnClickListener {
+
 
     override val viewModel: MainViewModel by lazy {
         ViewModelProvider(this).get(MainViewModel::class.java)
     }
+    lateinit var chosenUser: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,12 +33,54 @@ class MainFragment : BaseFragment(), View.OnClickListener {
         super.onViewCreated(view, savedInstanceState)
 
         btn_play.setOnClickListener(this)
+        img_user1_avatar.setOnClickListener(this)
+        img_user2_avatar.setOnClickListener(this)
     }
 
     override fun onClick(view: View) {
-        viewModel.onButtonClicked(
-            f_user1_name.text?.toString(),
-            f_user2_name.text?.toString()
-        )
+        when (view.id) {
+            R.id.btn_play -> {
+                viewModel.onButtonClicked(
+                    f_user1_name.text?.toString(),
+                    f_user2_name.text?.toString()
+                )
+            }
+            R.id.img_user1_avatar -> {
+                chosenUser = img_user1_avatar
+                getPicture()
+            }
+            R.id.img_user2_avatar -> {
+                chosenUser = img_user2_avatar
+                getPicture()
+            }
+        }
     }
+
+    fun getPicture() {
+        getContext()?.let {
+            CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setCropShape(CropImageView.CropShape.RECTANGLE)
+                .setFixAspectRatio(true)
+                .setBorderLineColor(Color.GREEN)
+                .setGuidelinesColor(Color.GRAY)
+                .setAutoZoomEnabled(false)
+                .start(it, this)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            val result = CropImage.getActivityResult(data)
+            if (resultCode == RESULT_OK) {
+                chosenUser.setImageURI(result.uri)
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                val error = result.error
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/com/dowgalolya/gwenthelper/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/dowgalolya/gwenthelper/viewmodels/MainViewModel.kt
@@ -1,11 +1,13 @@
 package com.dowgalolya.gwenthelper.viewmodels
 
 import android.app.Application
+import androidx.lifecycle.LiveData
 import com.dowgalolya.gwenthelper.fragments.MainFragmentDirections
 import com.dowgalolya.gwenthelper.livedata.ViewAction
 
 
 class MainViewModel(application: Application) : BaseViewModel(application) {
+
 
     fun onButtonClicked(user1Name: String?, user2Name: String?) {
         val direction = MainFragmentDirections.actionMainFragmentToGameFragment()
@@ -16,5 +18,9 @@ class MainViewModel(application: Application) : BaseViewModel(application) {
             direction.user2 = user2Name
         }
         _viewAction.value = ViewAction.NavigateWithDirection(direction)
+    }
+
+    fun onPhotoClicked(userId: Int) {
+
     }
 }
